ce448e2fe0d1c58cecec73b6623cc51ff6485e06
tutaru99 <simas.raudonis@gmail.com>
2021-05-14 13:35:25 +0300

    ci cd deployment

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/api/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
