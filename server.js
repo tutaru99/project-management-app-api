4d452d0a21dbe9b95639f49c4b3913ccd3dfa1ae
tutaru99 <simas.raudonis@gmail.com>
2021-05-14 15:47:00 +0300

    cors fixed - works for localhost & heroku

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const passport = require('passport');
-require('dotenv').config();
-require('./config/passport.js');
+const passport = require("passport");
+require("dotenv").config();
+require("./config/passport.js");
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
+
 const corsConfig = {
   credentials: true,
   origin: true,
 };
 app.use(cors(corsConfig));
-//swagger dependencies
-const swaggerUi = require('swagger-ui-express');
-const yaml = require('yamljs');
 
-//setup swagger
-const swaggerDefinition = yaml.load('./swagger.yaml');
-app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
-
-
-// app.use(
-//   cors({
-//     origin: "https://sn-project-management-app.herokuapp.com/",
-//     credentials: true,
-//   })
-// );
+/* allow localhost only */
 // app.use(cors(
 //   {
 //   origin: 'http://localhost:8080',
 //   credentials: true }
 //   ));
 
+//swagger dependencies
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+
+//setup swagger
+const swaggerDefinition = yaml.load("./swagger.yaml");
+app.use("/api/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
 // app.use('/api/user', passport.authenticate('jwt', {session: false}), authRoutes)
-app.use('/api/user', authRoutes)
-// simple route
+app.use("/api/user", authRoutes);
+
+// Hi route
 app.get("/api/", (req, res) => {
   res.json({ message: "Welcome to the Project Management Application" });
 });
@@ -55,12 +53,12 @@ const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database succesfully!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
